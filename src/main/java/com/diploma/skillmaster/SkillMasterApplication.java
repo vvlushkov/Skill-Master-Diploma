a07ff3b15b1526339d016e365d83554bd32e6143
Victor Lushkov <viktor20010627@gmail.com>
2023-05-29 19:03:21 +0200

    Initial commit

---
+++ src/main/java/com/diploma/skillmaster/SkillMasterApplication.java
@@ -0,0 +1,13 @@
+package com.diploma.skillmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillMasterApplication.class, args);
+	}
+
+}
